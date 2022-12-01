9252e2dc6c56234876360cce06778dc24b0b66fa
Alex Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-01 14:53:53 +0200

    Update Campaign.test.js

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -121,23 +121,3 @@ describe('Campaigns', () => {
     assert(balanceInEth > 103)
   })
 });
-
-
-// HOW TO THINK ABOUT TEST (my thoughts)
-
-// PREPARE
-// We test one complete task with multiple steps ("make an order" for example. which include in itself multiple steps
-// of choose item, put it inside cart, proceed to checkout, etc.)
-
-// First we write down in comments all-all steps from every contract's start to the end
-// we need to do to create our e2e test (to see all important functions in process)
-
-// Inside e2e stays only key steps of the process
-// (without checks if smth exists somewhere after action this checks will be done in separate tests)
-
-// WRITE ACTUAL CODE
-// We need to make sure our contracts are deployed properly. Usually it will be wrote inside beforeEach()
-//
-
-// Check other functions that can be exist, but was not used when we were write our initial e2e test guide
-
